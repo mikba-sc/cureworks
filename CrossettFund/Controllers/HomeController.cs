07e8f533780bafb31fb5a3397a800e1715e9e703
Barta <mbarta@childrens.sea.kids>
2018-02-27 09:49:37 -0800

    crosset test

---
+++ CrossettFund/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace crosset.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("", Order = 1)]
+        [Route("default.asp", Order = 2)]
+        public ActionResult Index()
+        {
+            ViewBag.pageType = "home";
+            return View();
+        }
+
+        [Route("projects.asp")]
+        public ActionResult Projects()
+        {
+            ViewBag.pageType = "projects";
+            return View();
+        }
+
+        [Route("highlight.asp")]
+        public ActionResult Highlights()
+        {
+            ViewBag.pageType = "highlight";
+            return View();
+        }
+
+        [Route("guidelines.asp")]
+        public ActionResult Guidelines()
+        {
+            ViewBag.pageType = "guidelines";
+            return View();
+        }
+
+        [Route("application.asp")]
+        public ActionResult Application()
+        {
+            ViewBag.pageType = "application";
+            return View();
+        }
+
+        [Route("contact.asp")]
+        public ActionResult Contact()
+        {
+            ViewBag.pageType = "contact";
+            return View();
+        }
+    }
+}
---
+++ CrossettFund/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(crosset.Startup))]
+namespace crosset
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          
+        }
+    }
+}
